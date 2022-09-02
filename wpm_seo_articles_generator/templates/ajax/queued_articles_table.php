bd86182da9a38df1b46cc58fec032b71c8598fc4
Oleg Shumar <oleg.shumar@gmail.com>
2022-09-02 15:48:35 +0200

    Update labels

--- wpm_seo_articles_generator/templates/ajax/queued_articles_table.php
+++ wpm_seo_articles_generator/templates/ajax/queued_articles_table.php
@@ -15,7 +15,7 @@
 					?></td>
                 <td><?php if(is_string($category)) { echo esc_html($category); } else { echo esc_html('Not found'); } ?></td>
                 <td><?php if($article->article_content != '') { ?> <a class="wpm-button-black" href="<?php if($article->post_id == 0) { ?>https://ai.wp-masters.com?view_article=<?php echo esc_attr($article->article_name); ?>&activation_key=<?php if(isset($settings['activation_key'])) { echo esc_attr($settings['activation_key']); } else { echo esc_attr('FREE'); } ?><?php } else { echo esc_attr(get_permalink($article->post_id)); } ?>" target="_blank">View Article</a>
-	                <?php if($article->post_id == 0) { ?><a class="wpm-button-black wpm-click-remove" href="<?php echo esc_attr(get_home_url()."?import_ai_post={$article->id}"); ?>" target="_blank">Import Post</a><?php } ?> <?php } elseif($article->errors != '') { ?><a class="wpm-button-black wpm-red-btn modal-view-content" href="#modal-content" rel="modal:open" data-title="<?php echo esc_attr($article->article_name); ?>" data-article-content="<?php echo esc_html(implode(', ', $article->errors)); ?>">View Errors</a><?php } else { echo esc_html("In Process"); } ?></td>
+	                <?php if($article->post_id == 0) { ?><a class="wpm-button-black wpm-click-remove" href="<?php echo esc_attr(get_home_url()."?import_ai_post={$article->id}"); ?>" target="_blank">Import Post</a><?php } ?> <?php } elseif($article->errors != '') { ?><a class="wpm-button-black wpm-red-btn modal-view-content" href="#modal-content" rel="modal:open" data-title="<?php echo esc_attr($article->article_name); ?>" data-article-content="<?php echo esc_html(implode(', ', $article->errors)); ?>">View Errors</a><?php } else { echo esc_html("In Progress"); } ?></td>
                 <td><?php if($article->date_posted != '' && strtotime($article->date_posted) > 0) {
 						if(get_option('date_format')) {
 							echo esc_html(date(get_option('date_format'), strtotime($article->date_posted)));
@@ -23,12 +23,12 @@
 							echo esc_html(date('Y-m-d H:i:s', strtotime($article->date_posted)));
 						}
 					} else {
-						echo esc_html('Not Posted');
+						echo esc_html('Not posted yet');
 					} ?></td>
             </tr>
 		<?php }} else { ?>
         <tr>
-            <td>No articles is sent to Assistant</td>
+            <td>No articles sent to Assistant yet</td>
         </tr>
 	<?php } ?>
     </tbody>
